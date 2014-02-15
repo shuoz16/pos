73dbe16ca34b17bbe945cc1a9ca539b525caf6e5
Jack Zhang <shuoz16@gmail.com>
2014-02-15 14:12:00 +1100

    update index.html

---
+++ pos/js/ui/main.js
@@ -0,0 +1,21 @@
+/*  jQuery ready function. Specify a function to execute when the DOM is fully loaded.  */
+$(document).ready(
+  /* This is the function that will get executed after the DOM is fully loaded */
+  function () {
+	$('#heading').hide();
+    /* Next part of code handles hovering effect and submenu appearing */
+    $('#nav li').hover(
+      function () { //appearing on hover
+        $('#heading').show();
+      },
+      function () { //disappearing on hover
+    	  $('#heading').hover(function () { $('#heading').show(); }, function () { $('#heading').hide(); });
+      }
+    );
+  }
+  
+  
+  
+  
+  
+);
